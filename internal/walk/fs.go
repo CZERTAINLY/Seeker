6a2b0fe9d46764b7d8028339027edf9b3826d56a
230388763 <230388763+plainsof@users.noreply.github.com>
2025-09-08 21:57:52 +0200

    feat(walk): add a module for filesystem and OCI Images traversal
    1. Filesystem traversal uses new `*os.Root` mechanism to anchor all
       lookups to defined root.
    2. OCI Images support is built on top of github.com/anchore/stereoscope
       providing an unified interface for container sources (registry,
       docker daemon, tarballs, ...)
    3. Iterator based API. Both `walk.FS` and `walk.Image` returns
       `iter.Seq2[walk.Entry, error]` which leads to more idiomatic API
       and unified access for both resources.
    4. Context aware - both iterators are stopped once provided context is
       canceled.
    5. Regular files only - due the specialized nature of a seeker, both
       iterators return only regular files keeping consumer code simpler.
    6. Unit tests - both `walk.FS` and `walk.Image` are tested.

---
+++ internal/walk/fs.go
@@ -0,0 +1,81 @@
+package walk
+
+import (
+	"context"
+	"io"
+	"io/fs"
+	"iter"
+	"os"
+	"path/filepath"
+)
+
+// FS returns an iterator over a filesystem - it returns an iterator for every
+// regular file or an error it visits.
+// It DOES NOT follow symlink. It DOES NOT open a file. Visits are done if all
+// files are visited or when the context is canceled.
+func FS(ctx context.Context, root *os.Root) iter.Seq2[Entry, error] {
+	if root == nil {
+		panic("root is nil")
+	}
+	return func(yield func(Entry, error) bool) {
+		_ = fs.WalkDir(root.FS(), ".", func(path string, d fs.DirEntry, err error) error {
+			if ctx.Err() != nil {
+				return fs.SkipAll
+			}
+			var entry = fsEntry{
+				root: root,
+				path: path,
+			}
+			var yieldErr error
+			if err != nil {
+				yieldErr = err
+			} else {
+				info, err := d.Info()
+				if err != nil {
+					entry.infoErr = err
+					yieldErr = err
+				} else {
+					if !info.Mode().IsRegular() {
+						return nil
+					}
+					entry.info = info
+					yieldErr = nil
+				}
+			}
+
+			if !yield(entry, yieldErr) {
+				return fs.SkipAll
+			}
+
+			return nil
+		})
+	}
+}
+
+// fsEntry implements Entry for a filesystem
+// it uses root.Open to open the file
+type fsEntry struct {
+	root    *os.Root
+	path    string
+	info    fs.FileInfo
+	infoErr error
+}
+
+// returns the absolute path to the file
+func (e fsEntry) Path() string {
+	return filepath.Join(e.root.Name(), e.path)
+}
+
+func (e fsEntry) Open() (io.ReadCloser, error) {
+	if e.infoErr != nil {
+		return nil, e.infoErr
+	}
+	if !e.info.Mode().IsRegular() {
+		return nil, nil
+	}
+	return e.root.Open(e.path)
+}
+
+func (e fsEntry) Stat() (fs.FileInfo, error) {
+	return e.info, e.infoErr
+}
