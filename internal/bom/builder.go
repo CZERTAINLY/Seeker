8952148689ef9dac9fc62a96f91af082835a35f4
230388763 <230388763+plainsof@users.noreply.github.com>
2025-09-13 19:15:28 +0200

    Add BOM building code
    New API for the process of building the CBOM itself. The
    `cyclone-dx` itself has no such API. This adds a builder pattern
    allowing BOM to be gradually updated with new data. Add an initial
    header and a metadata.

---
+++ internal/bom/builder.go
@@ -0,0 +1,109 @@
+package bom
+
+import (
+	"io"
+	"runtime/debug"
+	"time"
+
+	cdx "github.com/CycloneDX/cyclonedx-go"
+	"github.com/google/uuid"
+)
+
+var version string
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		version = "unknown"
+	} else {
+		version = info.Main.Version
+	}
+}
+
+// Builder is a builder pattern for a CycloneDX BOM structure
+type Builder struct {
+	authors    []cdx.OrganizationalContact
+	components []cdx.Component
+	properties []cdx.Property
+}
+
+func NewBuilder() *Builder {
+	return &Builder{}
+}
+
+func (b *Builder) AppendAuthors(authors ...cdx.OrganizationalContact) *Builder {
+	b.authors = append(b.authors, authors...)
+	return b
+}
+
+func (b *Builder) AppendComponents(components ...cdx.Component) *Builder {
+	b.components = append(b.components, components...)
+	return b
+}
+
+func (b *Builder) AppendProperties(properties ...cdx.Property) *Builder {
+	b.properties = append(b.properties, properties...)
+	return b
+}
+
+// BOM returns a cdx.BOM based on a data inside the Builder
+func (b *Builder) BOM() cdx.BOM {
+	bom := cdx.BOM{
+		JSONSchema:   "",
+		BOMFormat:    "CycloneDX",
+		SpecVersion:  cdx.SpecVersion1_6,
+		SerialNumber: "urn:uuid:" + uuid.New().String(),
+		Version:      1,
+		Metadata: &cdx.Metadata{
+			Timestamp: time.Now().Format(time.RFC3339),
+			Lifecycles: &[]cdx.Lifecycle{
+				{
+					Name:        "",
+					Phase:       "operations",
+					Description: "",
+				},
+			},
+			Authors: &b.authors,
+			// This can't be not nil otherwise this error will happen
+			// json: error calling MarshalJSON for type *cyclonedx.ToolsChoice: unexpected end of JSON input
+			Tools: nil,
+			Component: &cdx.Component{
+				Type:    "application",
+				Name:    "Seeker",
+				Version: version,
+				Manufacturer: &cdx.OrganizationalEntity{
+					BOMRef:  "",
+					Name:    "CZERTAINLY",
+					Address: &cdx.PostalAddress{},
+					URL: &[]string{
+						"https://www.czertainly.com",
+					},
+					Contact: &[]cdx.OrganizationalContact{},
+				},
+			},
+			Manufacture:  &cdx.OrganizationalEntity{},
+			Manufacturer: &cdx.OrganizationalEntity{},
+			Supplier:     &cdx.OrganizationalEntity{},
+			Licenses:     &cdx.Licenses{},
+			Properties:   &[]cdx.Property{},
+		},
+		Components:         &b.components,
+		Services:           &[]cdx.Service{},
+		ExternalReferences: &[]cdx.ExternalReference{},
+		Dependencies:       &[]cdx.Dependency{},
+		Compositions:       &[]cdx.Composition{},
+		Properties:         &b.properties,
+		Vulnerabilities:    &[]cdx.Vulnerability{},
+		Annotations:        &[]cdx.Annotation{},
+		Formulation:        &[]cdx.Formula{},
+		Declarations:       &cdx.Declarations{},
+		Definitions:        &cdx.Definitions{},
+	}
+	return bom
+}
+
+// AsJSON encode the BOM into JSON format
+func (b *Builder) AsJSON(w io.Writer) error {
+	bom := b.BOM()
+	return cdx.NewBOMEncoder(w, cdx.BOMFileFormatJSON).SetPretty(true).Encode(&bom)
+}
---
+++ internal/bom/builder_test.go
@@ -0,0 +1,81 @@
+package bom_test
+
+import (
+	"testing"
+
+	"github.com/CZERTAINLY/Seeker/internal/bom"
+	cdx "github.com/CycloneDX/cyclonedx-go"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuilder(t *testing.T) {
+	t.Parallel()
+
+	b := bom.NewBuilder().
+		AppendAuthors(cdx.OrganizationalContact{
+			BOMRef: "",
+			Name:   "test-author",
+			Email:  "test.author@example.net",
+			Phone:  "+1 555 555 5555",
+		}).
+		AppendComponents(cdx.Component{
+			BOMRef:       "",
+			MIMEType:     "application/x-pem-file",
+			Type:         "cryptographic-asset",
+			Supplier:     &cdx.OrganizationalEntity{},
+			Manufacturer: &cdx.OrganizationalEntity{},
+			Author:       "",
+			Authors:      &[]cdx.OrganizationalContact{},
+			Publisher:    "",
+			Group:        "",
+			Name:         "cert.pem",
+			Version:      "",
+			Description:  "",
+			Scope:        "",
+			Hashes: &[]cdx.Hash{
+				{
+					Algorithm: "sha256",
+					Value:     "??",
+				},
+			},
+			Licenses:           &cdx.Licenses{},
+			Copyright:          "",
+			CPE:                "",
+			PackageURL:         "",
+			OmniborID:          &[]string{},
+			SWHID:              &[]string{},
+			SWID:               &cdx.SWID{},
+			Modified:           new(bool),
+			Pedigree:           &cdx.Pedigree{},
+			ExternalReferences: &[]cdx.ExternalReference{},
+			Properties:         &[]cdx.Property{},
+			Components:         &[]cdx.Component{},
+			Evidence:           &cdx.Evidence{},
+			ReleaseNotes:       &cdx.ReleaseNotes{},
+			ModelCard:          &cdx.MLModelCard{},
+			Data:               &cdx.ComponentData{},
+			CryptoProperties: &cdx.CryptoProperties{
+				AssetType:           "certificate",
+				AlgorithmProperties: &cdx.CryptoAlgorithmProperties{},
+				CertificateProperties: &cdx.CertificateProperties{
+					SubjectName:           "CN=CommonNameOrHostname",
+					IssuerName:            "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName",
+					NotValidBefore:        "",
+					NotValidAfter:         "",
+					SignatureAlgorithmRef: "crypto/algorithm/sha-512-rsa@1.2.840.113549.1.1.13",
+					SubjectPublicKeyRef:   "",
+					CertificateFormat:     "X.509",
+					CertificateExtension:  "crt",
+				},
+				RelatedCryptoMaterialProperties: &cdx.RelatedCryptoMaterialProperties{},
+				ProtocolProperties:              &cdx.CryptoProtocolProperties{},
+				OID:                             "",
+			},
+		}).AppendProperties(cdx.Property{
+		Name:  "property1",
+		Value: "value1",
+	})
+
+	err := b.AsJSON(t.Output())
+	require.NoError(t, err)
+}
