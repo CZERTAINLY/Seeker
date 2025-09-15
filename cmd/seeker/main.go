2f72b570deed90ab72c8ceaa03ad6fd998f17ec0
230388763 <230388763+plainsof@users.noreply.github.com>
2025-09-15 21:07:24 +0200

    Read config flags from CLI, config file and environment variables
    Adds spf13/viper to ensure all three options remains in sync.

--- cmd/seeker/main.go
+++ cmd/seeker/main.go
@@ -5,17 +5,55 @@ import (
 	"iter"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
 
 	"github.com/CZERTAINLY/Seeker/internal/bom"
 	"github.com/CZERTAINLY/Seeker/internal/scan"
 	"github.com/CZERTAINLY/Seeker/internal/walk"
 	"github.com/CZERTAINLY/Seeker/internal/x509"
+
 	"github.com/anchore/stereoscope"
 	"github.com/anchore/stereoscope/pkg/image"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+var (
+	flagConfigFilePath string // value of --config flag
+	defaultConfigPath  string // /default/config/path/seeker on given OS
+	configPathUsed     string // actual config file used (if loaded)
+)
+
+func init() {
+	d, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+	defaultConfigPath = filepath.Join(d, "seeker")
+}
+
 func main() {
+	cobra.OnInitialize(initializeConfig)
+
+	// root flags
+	rootCmd.PersistentFlags().StringVar(&flagConfigFilePath, "config", "", "Config file to load - default is seeker.yaml in current directory or in "+defaultConfigPath)
+	// root sub-commands
+	rootCmd.AddCommand(alphaCmd)
+	rootCmd.AddCommand(versionCmd)
+
+	// alpha commands
+	alphaCmd.AddCommand(scanCmd)
+
+	// seeker alpha scan
+	// -path
+	scanCmd.Flags().String("path", ".", "local path to inspect")
+	_ = viper.BindPFlag("alpha.scan.path", scanCmd.Flags().Lookup("path"))
+	// - docker
+	scanCmd.Flags().String("docker", "", "docker image to inspect, must be pulled-in")
+	_ = viper.BindPFlag("alpha.scan.docker", scanCmd.Flags().Lookup("docker"))
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
@@ -23,15 +61,15 @@ func main() {
 
 func doScan(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Context()
-	flagPath, _ := cmd.Flags().GetString("path")
-	flagDocker, _ := cmd.Flags().GetString("docker")
+	flagPath := viper.GetString("alpha.scan.path")
+	flagDocker := viper.GetString("alpha.scan.docker")
 
 	var source iter.Seq2[walk.Entry, error]
 
 	if flagDocker == "" {
 		root, err := os.OpenRoot(flagPath)
 		if err != nil {
-			return fmt.Errorf("can't open -path %s: %w", flagPath, err)
+			return fmt.Errorf("can't open path %s: %w", flagPath, err)
 		}
 		source = walk.Root(ctx, root)
 	} else {
@@ -42,7 +80,7 @@ func doScan(cmd *cobra.Command, args []string) error {
 			nil,
 		)
 		if err != nil {
-			return fmt.Errorf("can't open -docker image, please docker pull %s first: %w", flagDocker, err)
+			return fmt.Errorf("can't open docker image, please docker pull %s first: %w", flagDocker, err)
 		}
 		source = walk.Image(ctx, ociImage)
 	}
@@ -83,17 +121,53 @@ var scanCmd = &cobra.Command{
 	RunE:  doScan,
 }
 
-func init() {
-	// root flags
-	// root commands
-	rootCmd.AddCommand(alphaCmd)
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "version provide version of a seeker",
+	Run: func(cmd *cobra.Command, args []string) {
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("seeker: version info not available")
+		}
 
-	// alpha commands
-	alphaCmd.AddCommand(scanCmd)
+		fmt.Printf("seeker: %s\n", info.Main.Version)
+		fmt.Printf("go:     %s\n", info.GoVersion)
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				fmt.Printf("commit: %s\n", s.Value)
+			case "vcs.time":
+				fmt.Printf("date:   %s\n", s.Value)
+			case "vcs.modified":
+				fmt.Printf("dirty:  %s\n", s.Value)
+			}
+		}
+		fmt.Println()
+	},
+}
 
-	// seeker alpha scan
-	// -path
-	scanCmd.Flags().String("path", ".", "local path to inspect")
-	// - docker
-	scanCmd.Flags().String("docker", "", "docker image to inspect, must be pulled-in")
+func initializeConfig() {
+	// use
+	if flagConfigFilePath != "" {
+		// 1.) passed --config path, so load the file
+		viper.SetConfigFile(flagConfigFilePath)
+	} else if envConfig, ok := os.LookupEnv("SEEKERCONFIG"); ok {
+		// 2.) or use SEEKERCONFIG - no underscore to not confuse viper
+		viper.SetConfigFile(envConfig)
+	} else {
+		// 3.) try to load seeker.yaml from current directory or default path for config files
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(defaultConfigPath)
+		viper.SetConfigName("seeker")
+		viper.SetConfigType("yaml")
+	}
+
+	// env variables are SEEKER with underscores
+	viper.SetEnvPrefix("SEEKER")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		configPathUsed = viper.ConfigFileUsed()
+	}
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/pavlo-v-chernykh/keystore-go/v4 v4.5.0
 	github.com/smallstep/pkcs7 v0.2.1
 	github.com/spf13/cobra v1.9.1
+	github.com/spf13/viper v1.21.0
 	github.com/stretchr/testify v1.11.1
 	github.com/testcontainers/testcontainers-go v0.38.0
 	go.uber.org/goleak v1.1.12
@@ -54,10 +55,12 @@ require (
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/ebitengine/purego v0.8.4 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
@@ -85,16 +88,21 @@ require (
 	github.com/opencontainers/runtime-spec v1.1.0 // indirect
 	github.com/opencontainers/selinux v1.11.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/scylladb/go-set v1.0.3-0.20200225121959-cc7b2070d91e // indirect
 	github.com/shirou/gopsutil/v4 v4.25.5 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/spf13/afero v1.14.0 // indirect
-	github.com/spf13/pflag v1.0.7 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/sylabs/sif/v2 v2.22.0 // indirect
 	github.com/sylabs/squashfs v1.0.6 // indirect
 	github.com/therootcompany/xz v1.0.1 // indirect
@@ -111,6 +119,7 @@ require (
 	go.opentelemetry.io/otel v1.36.0 // indirect
 	go.opentelemetry.io/otel/metric v1.36.0 // indirect
 	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
