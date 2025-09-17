0b0a1f1f3673926be2c24ca9137c8c459a9858c0
230388763 <230388763+plainsof@users.noreply.github.com>
2025-09-17 12:14:30 +0200

    Use a sync.Pool to reduce a number of repeated allocations
    Reduces the number of allocations - as most of the files will be read
    and rejected, this will reduce a number of unnecessary allocations.
    
    Add a tiny debug log showing what was done
    
    ```
    2025/09/17 12:13:43 DEBUG: pool: new=3 put=374, put(err)=0
    2025/09/17 12:13:43 DEBUG: scanned 396 files, appended 1 detections
    ```

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"iter"
+	"log"
+	"sync"
+	"sync/atomic"
 
 	"github.com/CZERTAINLY/Seeker/internal/model"
 	"github.com/CZERTAINLY/Seeker/internal/walk"
@@ -18,17 +20,30 @@ type Detector interface {
 }
 
 type Scan struct {
-	limit        int
-	skipIfBigger int64
-	detectors    []Detector
+	limit             int
+	skipIfBigger      int64
+	detectors         []Detector
+	pool              sync.Pool
+	poolNewCounter    atomic.Int32
+	poolPutCounter    atomic.Int32
+	poolPutErrCounter atomic.Int32
 }
 
-func New(limit int, detectors []Detector) Scan {
-	return Scan{
+func New(limit int, detectors []Detector) *Scan {
+	const skipIfBigger = 10 * 1024 * 1024
+	s := &Scan{
 		limit:        limit,
-		skipIfBigger: 10 * 1024 * 1024,
+		skipIfBigger: skipIfBigger,
 		detectors:    detectors,
 	}
+	s.pool = sync.Pool{
+		New: func() any {
+			s.poolNewCounter.Add(1)
+			b := make([]byte, skipIfBigger)
+			return &b
+		},
+	}
+	return s
 }
 
 // Do reads the content of the seq iterator and runs scanning on the entries
@@ -36,11 +51,11 @@ func New(limit int, detectors []Detector) Scan {
 // 2. If is bigger than 10MB, it's ignored and ErrTooBig is returned
 // 3. Otherwise the data are passed to the worker pool for running a detections
 // 4. Returns an iterator with a detections or Open/Read error or a ErrNoMatch if not match is found
-func (s Scan) Do(parentCtx context.Context, seq iter.Seq2[walk.Entry, error]) iter.Seq2[[]model.Detection, error] {
+func (s *Scan) Do(parentCtx context.Context, seq iter.Seq2[walk.Entry, error]) iter.Seq2[[]model.Detection, error] {
 	return newParallelMap(parentCtx, s.limit, s.scan).iter(seq)
 }
 
-func (s Scan) scan(ctx context.Context, entry walk.Entry) ([]model.Detection, error) {
+func (s *Scan) scan(ctx context.Context, entry walk.Entry) ([]model.Detection, error) {
 	if ctx.Err() != nil {
 		return nil, ctx.Err()
 	}
@@ -60,15 +75,25 @@ func (s Scan) scan(ctx context.Context, entry walk.Entry) ([]model.Detection, er
 		_ = f.Close() // ignoring close error for CLI tool
 	}()
 
-	b, err := io.ReadAll(f)
+	bp := s.pool.Get().(*[]byte)
+	buf := *bp
+	clear(buf)
+	n, err := f.Read(buf)
 	if err != nil {
+		s.poolPutErrCounter.Add(1)
+		s.pool.Put(bp)
 		return nil, fmt.Errorf("scan ReadAll: %w", err)
 	}
+	// IMPORTANT: data must be passed as buf[:n] otherwise data from a previous
+	// file will be passed in
+	buf = buf[:n]
 
 	var detectionErrors []error
 	res := make([]model.Detection, 0, 10)
 	for _, detector := range s.detectors {
-		d, err := detector.Detect(b, entry.Path())
+		d, err := detector.Detect(buf, entry.Path())
+		s.poolPutCounter.Add(1)
+		s.pool.Put(bp)
 		switch {
 		case err == nil:
 			res = append(res, d...)
@@ -84,6 +109,11 @@ func (s Scan) scan(ctx context.Context, entry walk.Entry) ([]model.Detection, er
 	} else if len(detectionErrors) > 0 {
 		return nil, errors.Join(detectionErrors...)
 	}
+
+	// DEBUG log to see how the pool is working, needs to be removed or replaced
+	// by slog
+	log.Printf("DEBUG: pool: new=%d put=%d, put(err)=%d\n", s.poolNewCounter.Load(), s.poolPutCounter.Load(), s.poolPutErrCounter.Load())
+
 	return res, nil
 }
 
