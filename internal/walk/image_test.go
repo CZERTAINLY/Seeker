4a8d20e75656df865da6b0727bf55dcf1bc8c604
230388763 <230388763+plainsof@users.noreply.github.com>
2025-11-19 22:26:49 +0100

    fix: do not segfault if docker host is not accessible
    Additionally it adds the unix:// prefix to absolute paths like
    /var/run/docker.sock on !windows systems.

---
+++ internal/walk/image_test.go
@@ -0,0 +1,29 @@
+package walk_test
+
+import (
+	"testing"
+
+	"github.com/CZERTAINLY/Seeker/internal/model"
+	"github.com/CZERTAINLY/Seeker/internal/walk"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWrongHost(t *testing.T) {
+	config := model.ContainerConfig{
+		Name: t.Name(),
+		Type: "docker",
+		// no unix:// prefix, but this won't be a valid path anyway
+		Host:   "#!var/run/not-a-docker.sock",
+		Images: nil,
+	}
+
+	// the goal of this test is to not segfault😃
+	idx := 0
+	for entry, err := range walk.Images(t.Context(), []model.ContainerConfig{config}) {
+		require.Nil(t, entry)
+		require.Error(t, err)
+		idx++
+	}
+	require.Equal(t, 1, idx)
+}
